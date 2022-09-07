c52e94137802a6d09b73483a90cad2335bc55cdc
Lokhtin Denis <denis.lokhtin@gmail.com>
2022-09-07 14:04:36 +0600

    create function getMonth

---
+++ src/middlewares/getMonth.js
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs';
+
+const getMonth = (month = dayjs().month()) => {
+    const year = dayjs().year();
+    const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
+    let currentMonthCount = 0 - firstDayOfTheMonth;
+
+    return new Array(5).fill([]).map(() => {
+        return new Array(7).fill(null).map(() => {
+            currentMonthCount++;
+            return dayjs(new Date(year, month, currentMonthCount));
+        });
+    });
+};
+
+export default getMonth;
